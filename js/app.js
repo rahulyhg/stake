c74d769b83535e5188550d6c20f2781678643328
wohlig <chintan@wohlig.com>
2014-12-15 15:57:35 +0530

    other
    other

--- js/app.js
+++ js/app.js
@@ -7,171 +7,158 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform,Books,$location) {
-	//console.log(Books.viewallbooks());
-	
-	
-		
-	
-	
-	
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-	
-	var db=Books.getdb();
-	db.transaction(function (tx) {
-		var booklength=0;
-		console.log("database Loaded");
-   		tx.executeSql('SELECT * FROM BOOKS', [], function (tx, results)
-			{
-				booklength=results.rows.length;
-				for(var i=0;i<booklength;i++)
-				{
-					bookrow=results.rows.item(i);	
-					if(bookrow.date=="undefined")
-					{
-						bookrow.date="";
-					}
-					Books.createbook(bookrow.name,bookrow.date,[],bookrow.id);
-					//console.log(Books.viewallbooks());
-				}
-			});		
-		tx.executeSql('SELECT * FROM HORSES', [], function (tx, results)
-			{
-				horselength=results.rows.length;
-				for(var i=0;i<horselength;i++)
-				{
-					horserow=results.rows.item(i);
-					
-					Books.insertonlyhorse(horserow.book,horserow.name,horserow.id);
-					console.log(Books.viewallbooks());
-				}
-				
-			});			
-		
-		tx.executeSql('SELECT * FROM BETS', [], function (tx, results)
-			{
-				betlength=results.rows.length;
-				for(var i=0;i<betlength;i++)
-				{
-					betrow=results.rows.item(i);
-					
-					Books.createbet(betrow.book,betrow.favorite,betrow.backlay,betrow.odds,betrow.stake,betrow.id);
-					console.log(Books.viewallbooks());
-				}
-				
-			});				
-			
- 	}, null);
-	$location.path("/tab/dash2/");
-	
-  });
+.run(function ($ionicPlatform, Books, $location) {
+    //console.log(Books.viewallbooks());
+    
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+
+        var db = Books.getdb();
+        db.transaction(function (tx) {
+            var booklength = 0;
+            console.log("database Loaded");
+            tx.executeSql('SELECT * FROM BOOKS', [], function (tx, results) {
+                booklength = results.rows.length;
+                for (var i = 0; i < booklength; i++) {
+                    bookrow = results.rows.item(i);
+                    if (bookrow.date == "undefined") {
+                        bookrow.date = "";
+                    }
+                    Books.createbook(bookrow.name, bookrow.date, [], bookrow.id);
+                    //console.log(Books.viewallbooks());
+                }
+            });
+            tx.executeSql('SELECT * FROM HORSES', [], function (tx, results) {
+                horselength = results.rows.length;
+                for (var i = 0; i < horselength; i++) {
+                    horserow = results.rows.item(i);
+
+                    Books.insertonlyhorse(horserow.book, horserow.name, horserow.id);
+                    console.log(Books.viewallbooks());
+                }
+
+            });
+
+            tx.executeSql('SELECT * FROM BETS', [], function (tx, results) {
+                betlength = results.rows.length;
+                for (var i = 0; i < betlength; i++) {
+                    betrow = results.rows.item(i);
+
+                    Books.createbet(betrow.book, betrow.favorite, betrow.backlay, betrow.odds, betrow.stake, betrow.id);
+                    console.log(Books.viewallbooks());
+                }
+
+            });
+
+        }, null);
+        $location.path("/tab/dash2/");
+
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
     // setup an abstract state for the tabs directive
     .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
     })
 
     // Each tab has its own nav history stack:
 
     .state('tab.dash', {
-      url: '/dash',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
+        url: '/dash',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+            }
         }
-      }
     })
 
-   .state('tab.viewbook', {
-      url: '/viewbook/:BookId',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/viewbook.html',
-          controller: 'ViewBook'
+    .state('tab.viewbook', {
+        url: '/viewbook/:BookId',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/viewbook.html',
+                controller: 'ViewBook'
+            }
         }
-      }
     })
-	.state('tab.createbook', {
-      url: '/createbook',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/createbook.html',
-          controller: 'CreateBook'
-        }
-      }
-    })
-	.state('tab.viewbets', {
-      url: '/viewbets/:BookId',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/viewbets.html',
-          controller: 'ViewBets'
-        }
-      }
-    })
-	.state('tab.createbet', {
-      url: '/createbet/:BookId/:HorseId',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/createbet.html',
-          controller: 'CreateBet'
-        }
-      }
-    })
-	.state('tab.deletebook', {
-      url: '/deletebook/:BookId',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/deletebook.html',
-          controller: 'DeleteBook'
-        }
-      }
-    })
+        .state('tab.createbook', {
+            url: '/createbook',
+            views: {
+                'tab-dash': {
+                    templateUrl: 'templates/createbook.html',
+                    controller: 'CreateBook'
+                }
+            }
+        })
+        .state('tab.viewbets', {
+            url: '/viewbets/:BookId',
+            views: {
+                'tab-dash': {
+                    templateUrl: 'templates/viewbets.html',
+                    controller: 'ViewBets'
+                }
+            }
+        })
+        .state('tab.createbet', {
+            url: '/createbet/:BookId/:HorseId',
+            views: {
+                'tab-dash': {
+                    templateUrl: 'templates/createbet.html',
+                    controller: 'CreateBet'
+                }
+            }
+        })
+        .state('tab.deletebook', {
+            url: '/deletebook/:BookId',
+            views: {
+                'tab-dash': {
+                    templateUrl: 'templates/deletebook.html',
+                    controller: 'DeleteBook'
+                }
+            }
+        })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
 
 })
 
-.filter('totalcolor', function() {
-  return function(input) {
-	  if(input>0)
-    return 'balanced';
-	else if (input<0)
-	return 'assertive';
-	else 
-	return 'positive';
-  };
+.filter('totalcolor', function () {
+    return function (input) {
+        if (input > 0)
+            return 'balanced';
+        else if (input < 0)
+            return 'assertive';
+        else
+            return 'positive';
+    };
 })
-.filter('BackLay', function() {
-  return function(input) {
-	if(input==1)
-    return 'Lay';
-	else
-	return 'Back';
-  };
-})
+    .filter('BackLay', function () {
+        return function (input) {
+            if (input == 1)
+                return 'Lay';
+            else
+                return 'Back';
+        };
+    })
 
 ;
-
--- js/services.js
+++ js/services.js
@@ -3,178 +3,173 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Books', function() {
-  // Might use a resource here that returns a JSON array
-  // Some fake testing data
+.factory('Books', function () {
+    // Might use a resource here that returns a JSON array
+    // Some fake testing data
 
-	
-	  
-  var db = openDatabase('books', '1.0', 'Books Database', 2 * 1024 * 1024);
-  
-  db.transaction(function (tx) {  
-		tx.executeSql('CREATE TABLE IF NOT EXISTS BOOKS (id, name,date)');
-		tx.executeSql('CREATE TABLE IF NOT EXISTS HORSES (id, name,book)');
-		tx.executeSql('CREATE TABLE IF NOT EXISTS BETS (id, book,favorite,backlay,stake,odds,timestamp)');
-	});
-	
-	
-	
-  var books = [
+
+
+    var db = openDatabase('books', '1.0', 'Books Database', 2 * 1024 * 1024);
+
+    db.transaction(function (tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS BOOKS (id, name,date)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS HORSES (id, name,book)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS BETS (id, book,favorite,backlay,stake,odds,timestamp)');
+    });
+
+
+
+    var books = [
       ];
-	
-		
-	
-  return {
-	getdb: 
-	function () {
-		return db;
-	}
-	,
-	createbook: function(bookname,date,horses,id) {
-		var newbookid=0;
-		if(id)
-		{
-			newbookid=parseInt(id);
-		}
-		else
-		{
-			if(books.length>0)
-			newbookid=books[books.length-1].id+1;
-		}
-		
-		
-		var newhorses=[];
-		for(var i=0;i<horses.length;i++)
-		{	
-			if(horses[i].name!="")
-			{
-				newhorses.push({id:i,name:horses[i].name,total:0});
-			}
-		}
-		
-		books.push({id:newbookid,date:date,name:bookname,horses:newhorses,bets:[]});	
-		console.log(newhorses);
-		console.log(newbookid);
-		if(!id)
-		{
-			console.log("Database Insertion");
-			db.transaction(function (tx) {  
-				tx.executeSql('INSERT INTO BOOKS (id,name,date) VALUES ("'+newbookid+'","'+bookname+'","'+date+'")');
-				for(var i=0;i<newhorses.length;i++)
-				{
-					tx.executeSql('INSERT INTO HORSES (id, name,book) VALUES ("'+newhorses[i].id+'","'+newhorses[i].name+'","'+newbookid+'")');
-				}
-			}); 
-		}
-		
-		
-	},
-	deletebook: function(book) {
-		for(var i=0;i<books.length;i++)
-		{
-			if(book==books[i].id)
-			{
-				books.splice(i,1);
-			}
-		}
-		db.transaction(function (tx) {  
-				tx.executeSql('DELETE FROM BOOKS WHERE id="'+book+'"');
-				tx.executeSql('DELETE FROM HORSES WHERE book="'+book+'"');
-				tx.executeSql('DELETE FROM BETS WHERE book="'+book+'"');
-		}); 
-		
-     
-    },
-    viewallbooks: function() {
-      return books;
-    },
-	insertonlyhorse: function(book,horsename,horseid) {
-     	books[book].horses.push({id:horseid,name:horsename,total:0});
-    },
-    viewbook: function(BookId) {
-		for(var i=0;i<books.length;i++)
-		{
-			if(BookId==books[i].id)
-			{
-				return books[i];
-			}
-		}
-      	
-    },
-	createbet: function(book,favorite,backlay,odds,stake,id) {
-		favorite=parseInt(favorite);
-		backlay=parseInt(backlay);
-		odds=parseFloat(odds);
-		stake=parseFloat(stake);
-		var betnewid=0;
-		if(id)
-		{
-			betnewid=parseInt(id);
-		}
-		{
-			if(books[book].bets.length>0)
-			var betnewid=books[book].bets[books[book].bets.length-1].id+1;
-		}
-		var horses=books[book].horses;
-		var currentdate = new Date();
-		var currenttimestamp = currentdate.getTime();
-		books[book].bets.push({id:betnewid,favorite:favorite,stake:stake,backlay:backlay,odds:odds,timestamp:currenttimestamp});
-		var favstake=odds*stake*backlay;
-		var otherstake=stake*backlay*-1;
-		for(var i=0;i<horses.length;i++)
-		{
-			if(favorite==horses[i].id)
-			{
-				horses[i].total+=favstake;
-			}
-			else 
-			{
-				horses[i].total+=otherstake;
-			}
-		}
-		if(!id)
-		db.transaction(function (tx) {  
-				tx.executeSql('INSERT INTO BETS (id, book,favorite,backlay,stake,odds,timestamp) VALUES ("'+betnewid+'","'+book+'","'+favorite+'","'+backlay+'","'+stake+'","'+odds+'","'+currenttimestamp+'")');
-		}); 
-		return "Bet Added";
-	},
-	deletebet: function(book,betid) {
-		var bets=books[book].bets;
-		var horses=books[book].horses;
-		var favorite=0;
-		var backlay=0;
-		var odds=0;
-		var stake=0;
-		for(var i=0;i<bets.length;i++)
-		{
-			if(betid==bets[i].id)
-			{
-				favorite=bets[i].favorite;
-				backlay=bets[i].backlay;
-				odds=bets[i].odds;
-				stake=bets[i].stake;
-				books[book].bets.splice(i,1);
-			}
-		}
-		var favstake=odds*stake*backlay;
-		var otherstake=stake*backlay*-1;
-		for(var i=0;i<horses.length;i++)
-		{
-			if(favorite==horses[i].id)
-			{
-				horses[i].total-=favstake;
-			}
-			else 
-			{
-				horses[i].total-=otherstake;
-			}
-		}
-		
-		db.transaction(function (tx) {  
-				tx.executeSql('DELETE FROM BETS WHERE id="'+betid+'" AND book="'+book+'"');
-		}); 
-		
-	}
-  }
-})
-;
+
+
+
+    return {
+        getdb: function () {
+            return db;
+        },
+        createbook: function (bookname, date, horses, id) {
+            var newbookid = 0;
+            if (id) {
+                newbookid = parseInt(id);
+            } else {
+                if (books.length > 0)
+                    newbookid = books[books.length - 1].id + 1;
+            }
+
+
+            var newhorses = [];
+            for (var i = 0; i < horses.length; i++) {
+                if (horses[i].name != "") {
+                    newhorses.push({
+                        id: i,
+                        name: horses[i].name,
+                        total: 0
+                    });
+                }
+            }
+
+            books.push({
+                id: newbookid,
+                date: date,
+                name: bookname,
+                horses: newhorses,
+                bets: []
+            });
+            console.log(newhorses);
+            console.log(newbookid);
+            if (!id) {
+                console.log("Database Insertion");
+                db.transaction(function (tx) {
+                    tx.executeSql('INSERT INTO BOOKS (id,name,date) VALUES ("' + newbookid + '","' + bookname + '","' + date + '")');
+                    for (var i = 0; i < newhorses.length; i++) {
+                        tx.executeSql('INSERT INTO HORSES (id, name,book) VALUES ("' + newhorses[i].id + '","' + newhorses[i].name + '","' + newbookid + '")');
+                    }
+                });
+            }
+
+
+        },
+        deletebook: function (book) {
+            for (var i = 0; i < books.length; i++) {
+                if (book == books[i].id) {
+                    books.splice(i, 1);
+                }
+            }
+            db.transaction(function (tx) {
+                tx.executeSql('DELETE FROM BOOKS WHERE id="' + book + '"');
+                tx.executeSql('DELETE FROM HORSES WHERE book="' + book + '"');
+                tx.executeSql('DELETE FROM BETS WHERE book="' + book + '"');
+            });
+
+
+        },
+        viewallbooks: function () {
+            return books;
+        },
+        insertonlyhorse: function (book, horsename, horseid) {
+            books[book].horses.push({
+                id: horseid,
+                name: horsename,
+                total: 0
+            });
+        },
+        viewbook: function (BookId) {
+            for (var i = 0; i < books.length; i++) {
+                if (BookId == books[i].id) {
+                    return books[i];
+                }
+            }
+
+        },
+        createbet: function (book, favorite, backlay, odds, stake, id) {
+            favorite = parseInt(favorite);
+            backlay = parseInt(backlay);
+            odds = parseFloat(odds);
+            stake = parseFloat(stake);
+            var betnewid = 0;
+            if (id) {
+                betnewid = parseInt(id);
+            } {
+                if (books[book].bets.length > 0)
+                    var betnewid = books[book].bets[books[book].bets.length - 1].id + 1;
+            }
+            var horses = books[book].horses;
+            var currentdate = new Date();
+            var currenttimestamp = currentdate.getTime();
+            books[book].bets.push({
+                id: betnewid,
+                favorite: favorite,
+                stake: stake,
+                backlay: backlay,
+                odds: odds,
+                timestamp: currenttimestamp
+            });
+            var favstake = odds * stake * backlay;
+            var otherstake = stake * backlay * -1;
+            for (var i = 0; i < horses.length; i++) {
+                if (favorite == horses[i].id) {
+                    horses[i].total += favstake;
+                } else {
+                    horses[i].total += otherstake;
+                }
+            }
+            if (!id)
+                db.transaction(function (tx) {
+                    tx.executeSql('INSERT INTO BETS (id, book,favorite,backlay,stake,odds,timestamp) VALUES ("' + betnewid + '","' + book + '","' + favorite + '","' + backlay + '","' + stake + '","' + odds + '","' + currenttimestamp + '")');
+                });
+            return "Bet Added";
+        },
+        deletebet: function (book, betid) {
+            var bets = books[book].bets;
+            var horses = books[book].horses;
+            var favorite = 0;
+            var backlay = 0;
+            var odds = 0;
+            var stake = 0;
+            for (var i = 0; i < bets.length; i++) {
+                if (betid == bets[i].id) {
+                    favorite = bets[i].favorite;
+                    backlay = bets[i].backlay;
+                    odds = bets[i].odds;
+                    stake = bets[i].stake;
+                    books[book].bets.splice(i, 1);
+                }
+            }
+            var favstake = odds * stake * backlay;
+            var otherstake = stake * backlay * -1;
+            for (var i = 0; i < horses.length; i++) {
+                if (favorite == horses[i].id) {
+                    horses[i].total -= favstake;
+                } else {
+                    horses[i].total -= otherstake;
+                }
+            }
+
+            db.transaction(function (tx) {
+                tx.executeSql('DELETE FROM BETS WHERE id="' + betid + '" AND book="' + book + '"');
+            });
+
+        }
+    }
+});
