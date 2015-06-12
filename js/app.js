ddd3d95c89da6bc3579d6f3ed18dd121ff719832
Tushar <tushar@wohlig.com>
2015-06-12 10:37:34 +0530

    Stake updates

--- js/app.js
+++ js/app.js
@@ -1,24 +1,15 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function ($ionicPlatform, Books, $location) {
+.run(function($ionicPlatform, Books, $location) {
     //console.log(Books.viewallbooks());
 
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
+    $ionicPlatform.ready(function() {
         if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
         }
         if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
+            StatusBar.overlaysWebView(true);
+            StatusBar.styleLightContent();
         }
         //
         //        var db = Books.getdb();
@@ -64,7 +55,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     });
 })
 
-.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
     $ionicConfigProvider.views.maxCache(0);
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
@@ -72,14 +63,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     // Each state's controller can be found in controllers.js
     $stateProvider
 
-    // setup an abstract state for the tabs directive
     .state('tab', {
         url: "/tab",
         abstract: true,
         templateUrl: "templates/tabs.html"
     })
-
-    // Each tab has its own nav history stack:
 
     .state('tab.dash', {
         url: '/dash',
@@ -142,8 +130,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 })
 
-.filter('totalcolor', function () {
-    return function (input) {
+.filter('totalcolor', function() {
+    return function(input) {
 
         input = parseInt(input);
         if (input > 0)
@@ -154,8 +142,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
             return 'positive';
     };
 })
-    .filter('BackLay', function () {
-        return function (input) {
+    .filter('BackLay', function() {
+        return function(input) {
             if (input == 1)
                 return 'Lay';
             else
